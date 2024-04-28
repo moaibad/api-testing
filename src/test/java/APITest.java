5443e64c1cb00f149768995ee3a17bf81691bcc3
moaibad <fathulibad.412@gmail.com>
2024-04-28 21:59:52 +0700

    add project and test example

---
+++ src/test/java/APITest.java
@@ -0,0 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class APITest {
+
+    String token = "662e5b1dbb70a783532595b0";
+
+    @Test
+    void testGetList(){
+
+        Response response = RestAssured.given()
+                .header("app-id", token)
+                .get("https://dummyapi.io/data/v1/user");
+
+        System.out.println("Response : " + response.asString());
+        System.out.println("Status : " + response.getStatusCode());
+        System.out.println("Body : " + response.getBody().asString());
+        System.out.println("Time taken : " + response.getTime());
+        System.out.println("Header : " + response.getHeader("content-type"));
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+    }
+}
